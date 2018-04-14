bbbaaaedf0625ef6c5fb3fa5729dc71a8da9e6a7
Léandre DAUMONT <kekos91@free.fr>
2018-04-14 21:36:16 +0200

    updated statusbar on android. again.

--- src/components/CustomStatusBar.js
+++ src/components/CustomStatusBar.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Platform, StatusBar, View } from 'react-native';
 
 const CustomStatusBar = (props) => {
-    let android = Platform.OS === "android";
-    let height = android ? 24 : 0;
+    let android = Platform.OS == "android";
     let color = props.color ? props.color : "transparent";
+
+    if (android) {
+        return (
+            <View style={{ height: 24 }}>
+                <StatusBar
+                    translucent
+                    backgroundColor={color}
+                    barStyle="dark-content"
+                />
+            </View>
+        );
+    }
     return (
-        <View style={{height: height}}>
-            <StatusBar
-                translucent
-                backgroundColor={color}
-                barStyle="dark-content"
-            />
-        </View>
+        <StatusBar
+            translucent
+            backgroundColor={color}
+            barStyle="dark-content"
+        />
     );
+
 };
 
 export default CustomStatusBar;
